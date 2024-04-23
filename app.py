fc25d0997e982a6ccc916f3aefafb00732d83d03
ian-healey_iress <ian.healey@iress.com>
2024-04-23 11:23:22 +0100

    Exercise 2

--- app.py
+++ app.py
@@ -2,12 +2,24 @@ print("Hello world")
 
 # Exercise 1
 
-books = {"author1" : ["book1", "book2", "book3"], "author2" : ["book1", "book2"]}
+# books = {"author1" : ["book1", "book2", "book3"], "author2" : ["book1", "book2"]}
 
-author = books.get(input("Please enter an author: "), "nope")
+# author = books.get(input("Please enter an author: "), "nope")
 
-if author != "nope":
-    authStr = ', '.join(author)
-    print(authStr)
+# if author != "nope":
+#     authStr = ', '.join(author)
+#     print(authStr)
+# else:
+#     print("nah homie")
+
+
+# Exercise 2
+
+grade = int(input("Enter Grade: "))
+
+if grade >= 85:
+    print("Distinction")
+elif grade >= 65:
+    print("Pass")
 else:
-    print("nah homie")
+    print("Fail")
