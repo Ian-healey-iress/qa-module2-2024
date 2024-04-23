ebaebf2b8f11beb5a436d8a2dfc6a0b5836f217c
ian-healey_iress <ian.healey@iress.com>
2024-04-23 09:57:16 +0100

    Exercise 1

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
 print("Hello world")
+
+# Exercise 1
+
+books = {"author1" : ["book1", "book2", "book3"], "author2" : ["book1", "book2"]}
+
+author = books.get("author1", "nope")
+str = ', '.join(author)
+
+print(str)
