aba1d7a0a4d0c9db581421767a3f28ef53c4280a
ian-healey_iress <ian.healey@iress.com>
2024-04-23 12:22:04 +0100

    Input validation and try except loop

--- app.py
+++ app.py
@@ -27,18 +27,29 @@ print("Hello world")
 
 #  Exercose 3
 
-weight = float(input("Enter Weight: "))
+while True:     
+    try:
+        weight = float(input("Enter Weight: "))
+        if ValueError == True:
+            raise ValueError
+        else:
+            break
+    except ValueError:
+        print("Please enter a numberic input")
 
 userIn = "invalid"
 
 while True:
     unit = input("Select unit (1 for kg or 2 for lbs): ")
     if unit == "1" or unit == "2":
+        if unit == "1":
+            converted = round(weight * 2.2, 1)
+            convertedUnit = "lbs"
+        else:
+            converted = round(weight / 2.2, 1)
+            convertedUnit = "kgs"
         break
     else:
-        print("Invalid input, please try again")
-        
-if unit == "1":
-    print(f"Your weight is {weight} kgs, or {round(weight * 2.2, 2)} lbs")
-else:
-    print(f"Your weight is {weight} lbs, or {round(weight / 2.2, 2)} kgs")
+        print("Invalid input, please try again")     
+
+print(f"your weight is {converted} {convertedUnit}")
