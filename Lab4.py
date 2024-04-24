3071d4dd5f3dd709611e1d5f427171a1ab8d952f
ian-healey_iress <ian.healey@iress.com>
2024-04-24 12:00:58 +0100

    Completed lab4

---
+++ Lab4.py
@@ -0,0 +1,121 @@
+def convertListToMins(timeList):
+    days = timeList[0]
+    hours = timeList[1]
+    mins = timeList[2]
+    newMins = mins + (hours * 60) + (days * 24 * 60)
+    return newMins
+
+def convertListToHours(timeList):
+    days = timeList[0]
+    hours = timeList[1]
+    mins = timeList[2]
+    newHours = (mins/60) + hours + (days * 24)
+    return newHours
+
+def convertUnitToList(timeIn, unit):
+    if unit == "hours":
+        timeIn *= 60
+    elif unit == "days":
+        timeIn *= 60
+        timeIn *= 24
+    hours = timeIn//60
+    leftoverMins = timeIn%60
+    days = hours//24
+    leftoverHours = hours%24
+    newList = [days, leftoverHours, leftoverMins]
+    return newList
+
+def convertListToString(list):
+    days = list[0]
+    hours = list[1]
+    minutes = list[2]
+    string = f"{round(days, 1)} days, {round(hours, 1)} hours, and {round(minutes, 1)} minutes"
+    return string
+
+def retrieveUserInputList():
+    formatList = ["Days", "Hours", "Minutes"]
+    while True:
+        try:
+            timeList = [float(input(f"Please enter value for {x}: ")) for x in formatList]
+            if ValueError == True:
+                raise ValueError
+            else:
+                return timeList
+        except ValueError:
+            print("Please enter a valid float input")
+
+def retrieveUserInput(unit):
+    while True:
+        try:
+            timeDivision = float(input(f"Please enter a number of {unit}: "))
+            if ValueError == True:
+                raise ValueError
+            else:
+                return timeDivision
+        except ValueError:
+            print("Please enter a valid float input")
+
+def addTwoTimes(time1, time2):
+    mins1 = convertListToMins(time1)
+    mins2 = convertListToMins(time2)
+    totalMins = mins1 + mins2
+    newTime = convertUnitToList(totalMins, "mins")
+    return newTime
+
+def differenceTwoTimes(time1, time2):
+    mins1 = convertListToMins(time1)
+    mins2 = convertListToMins(time2)
+    largerMins = max([mins1, mins2])
+    smallerMins = min([mins1, mins2])
+    differenceMins = largerMins - smallerMins
+    timeDifference = convertUnitToList(differenceMins, "mins")
+    return timeDifference
+
+
+menu = {
+    1 : "Add 2 times.",
+    2 : "Find difference between two times.",
+    3 : "Convert time to hours.",
+    4 : "Convert time to minutes.",
+    5 : "Convert minutes to time.",
+    6 : "Convert hours to time.",
+    7 : "Convert days to time.",
+    8 : "Exit."
+}
+
+for x in menu:
+    print(f"{list(menu.keys())[x-1]}.\t{list(menu.values())[x-1]}")
+
+while True:
+    try:
+        selected = int(input("Please select: "))
+        if ValueError == True or selected > 8 or selected < 0:
+            raise ValueError
+        else:
+            if selected == 1:
+                timeList1 = retrieveUserInputList()
+                timeList2 = retrieveUserInputList()
+                print(f"The two times add up to {convertListToString(addTwoTimes(timeList1, timeList2))}")
+            elif selected == 2:
+                timeList1 = retrieveUserInputList()
+                timeList2 = retrieveUserInputList()
+                print(f"The difference between the two times are {convertListToString(differenceTwoTimes(timeList1, timeList2))}")
+            elif selected == 3:
+                timeList = retrieveUserInputList()
+                print(f"The time given is {convertListToHours(timeList)} hours")
+            elif selected == 4:
+                timeList = retrieveUserInputList()
+                print(f"The time given is {convertListToMins(timeList)} minutes")
+            elif selected == 5:
+                mins = retrieveUserInput("minutes")
+                print(f"{mins} minutes converted into time is {convertListToString(convertUnitToList(mins, "mins"))}")
+            elif selected == 6:
+                hours = retrieveUserInput("hours")
+                print(f"{hours} hours converted into time is {convertListToString(convertUnitToList(hours, "hours"))}")
+            elif selected == 7:
+                days = retrieveUserInput("days")
+                print(f"{days} days converted into time is {convertListToString(convertUnitToList(days, "days"))}")
+            elif selected == 8:
+                quit()
+    except ValueError:
+        print("Please enter a valid input (1-8)")
--- app.py
+++ app.py
@@ -57,5 +57,62 @@ print("Hello world")
 
 # Exercise 4
 
-names = [print(f"{name} is great") for name in [input("Enter name: ") for x in range(5)]]
+# names = [print(f"{name} is great") for name in [input("Enter name: ") for x in range(5)]]
+
+
+# Exercise 5
+
+def findBudget():
+    while True:     
+        try:
+            budget = float(input("Enter Budget: "))
+            if ValueError == True:
+                raise ValueError
+            else:
+                return budget
+        except ValueError:
+            print("Please enter a numberic input")
+
+def retriveUserIn():
+    while True:
+        try:
+            selected = int(input("Please select 1, 2, or 3: "))
+            if ValueError == True or selected > 3 or selected < 0:
+                raise ValueError
+            else:
+                return selected
+        except ValueError:
+            print("Please enter a valid input (1, 2, or 3)")
+
+shakes = {
+    "Shake 1" : 10,
+    "Shake 2" : 8.50,
+    "Shake 3" : 6.45
+}
+
+budget = findBudget()
+
+print("Menu:")
+counter = 0
+for shake in shakes:
+    counter+=1
+    print(f"{counter}.\t{shake}")
+
+purchased = []
+
+while True:
+    selected = retriveUserIn()
+    if list(shakes.values())[selected - 1] > budget:
+        print(f"you cant afford that, your budget is only {budget}")
+    else:
+        purchased.append(str(list(shakes.keys())[selected - 1]))
+        budget -= list(shakes.values())[selected - 1]
+        print("So far you have bought:")
+        for x in purchased:
+            print(x)
+        print(f"you have {round(budget, 1)} left")
+
+    if min(list(shakes.values())) > budget:
+        print("You haven't got enough money to buy anything, you have been kicked out")
+        quit()
 
