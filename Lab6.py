9aee1eb5981c5310823ce8dd29b3da5e8ce54fbb
ian-healey_iress <ian.healey@iress.com>
2024-04-24 15:28:51 +0100

    Added lab 6

---
+++ Lab6.py
@@ -0,0 +1,22 @@
+def calculateTax(income):
+    if income <= 11500:
+        return "0"
+    if income > 11850:
+        tax = income * 0.2
+    if income > 34500:
+        tax = income * 0.4
+    if income > 150000:
+        tax = income * 0.45
+    return tax
+
+while True:
+    try:
+        income = float(input(f"Please enter your income: "))
+        if ValueError == True:
+            raise ValueError
+        else:
+            print(f"The total tax you must pay is {calculateTax(income)}")
+            break
+    except ValueError:
+        print("Please enter a valid float input")
+
