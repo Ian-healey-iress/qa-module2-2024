79c72cc808e72113f72223a676b5de0771bec35a
ian-healey_iress <ian.healey@iress.com>
2024-04-30 14:49:53 +0100

    Added more unit tests

--- passTests.py
+++ passTests.py
@@ -35,6 +35,22 @@ class TestPass(unittest.TestCase):
         self.assertEqual(passCheck.checkPassword(passwordStrong)[0], ('strong'))
         self.assertEqual(passCheck.checkPassword(passwordVeryStrong)[0], ('very strong'))
 
+    def test_passCheck_checkPassword_returns_correct_improvements(self):
+        improveList = ["Add special characters", "Add uppercase characters", "Add lowercase characters", "Add numbers", "Make sure password contains more than 8 charaters", "Refrain from using a password that is commonly used by others (password, password123, etc.)"]
+        passwordSpecial = Pass("Test1234")
+        passwordUpper = Pass("test1234!")
+        passwordLower = Pass("TEST1234!?!")
+        passwordNumbers = Pass("Test!?!?!")
+        passwordShort = Pass("Tes1!")
+        passwordCommon = Pass("Password")
+        passCheck = PassCheck()
+        self.assertEqual(passCheck.checkPassword(passwordSpecial)[1], [improveList[0]])
+        self.assertEqual(passCheck.checkPassword(passwordUpper)[1], [improveList[1]])
+        self.assertEqual(passCheck.checkPassword(passwordLower)[1], [improveList[2]])
+        self.assertEqual(passCheck.checkPassword(passwordNumbers)[1], [improveList[3]])
+        self.assertEqual(passCheck.checkPassword(passwordShort)[1], [improveList[4]])
+        self.assertEqual(passCheck.checkPassword(passwordCommon)[1], [improveList[5]])
+
 
 if __name__ == '__main__':
     unittest.main()
